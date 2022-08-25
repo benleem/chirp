2df6759b0c416ac7dd24fecf9383ddae430333b4
Ben Marshall <benleem98@yahoo.com>
2022-08-24 23:51:59 -0400

    changing manifest

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,8 @@ const Document = () => {
 				<meta charSet="utf-8" />
 				<link rel="icon" href="/img/logo.svg" />
 				<link rel="manifest" href="/manifest.json" />
-				<link rel="apple-touch-icon" href="/img/logo.svg" />
+				<link rel="apple-touch-icon" href="/logo-192x192.png" />
 				<meta name="theme-color" content="#f0f2f5" />
-				{/* <meta name="apple-mobile-web-app-capable" content="yes"></meta> */}
 				<meta
 					name="apple-mobile-web-app-status-bar-style"
 					content="black-translucent"
